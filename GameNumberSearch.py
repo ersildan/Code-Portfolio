8bcd0d437b4f3bd684786d5bb54b04d991fc61c5
11fre <11free11@mail.ru>
2025-05-04 09:36:51 +0300

    Changed text and rename file.

--- GameNumberSearch.py
+++ GameNumberSearch.py
@@ -5,26 +5,27 @@ import time
 
 colorama.init()
 def game_random():
-    print('Отгадай за 7 попыток.\nКакое число я загадал?')
+    print('Отгадай за 7 попыток.\nКакое число я загадал? от 1 до 100')
 
     total = 7
-    random_number = random.randrange(0, 101)
+    random_number = random.randrange(1, 101)
     while total != 0:
+        print()
         try:
             num = int(input())
-            if 0 <= num < random_number:
+            if 0 < num < random_number:
                 print(f'Слишком мало, попробуйте ещё раз\nПопыток осталось [ {total-1} ]')
-            elif random_number < num <= 100:
+            elif 101 > num > random_number:
                 print(f'Слишком много, попробуйте ещё раз\nПопыток осталось [ {total-1} ]')
-            elif random_number == num:
+            elif num == random_number:
                 print(f'Вы угадали, поздравляю!\nПопыток осталось [ {total-1} ]')
                 break
             else:
-                print('Я загадал число от 0 до 100, попробуйте снова')
+                print('Я загадал число от 1 до 100, попробуйте снова')
         except (TypeError, ValueError):
             print('Нужно вводить только число')
         total -= 1
-    return colorama.Fore.RED + "GameOver. "+ colorama.Fore.RESET + "К сожалению, ты не выиграл."
+    return colorama.Fore.RED + "К сожалению, вы проиграли."
 
 while True:
     print(colorama.Fore.RED + colorama.Fore.YELLOW + 'Сыграем? Нажми [ + ] или [ - ]')
