3fe407e52ac35d85c674e7e07b328c571e45e968
11fre <11free11@mail.ru>
2025-07-10 13:45:43 +0300

    +1

---
+++ withopen2.py
@@ -0,0 +1,14 @@
+def new_function(n, num):
+    if n > 3 or num < 30:
+        return 'Bad'
+    else:
+        with open('file.txt', 'w+', encoding='UTF-8') as f:
+            for el in range(1, n + 1):
+                f.write(f"{el} день - баланс {num} - списалось 7 - осталось {num - 7}\n")
+                num -= 7
+            f.seek(0)
+            txt = f.read()
+        return txt
+
+
+print(new_function(int(input()), int(input())))
