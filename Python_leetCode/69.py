6ffcf7114fd6bde41e7ae0d504897bad04e1e1c8
ersildan <11free11@mail.ru>
2025-09-15 09:53:48 +0300

    69. Sqrt(x)

---
+++ Python_leetCode/69.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(x**0.5)
+
+"""
+for el in x:
+    if i * i == x:
+        return i
+    elif i * i > x:
+        return i - 1 
+"""
