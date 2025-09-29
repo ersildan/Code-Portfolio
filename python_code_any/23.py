6c4a7498c65bdc7199fa72168b0ff7b5d06aa6c3
ersildan <11free11@mail.ru>
2025-09-29 17:58:46 +0300

    +

--- python_code_any/23.py
+++ python_code_any/23.py
@@ -1,15 +1,21 @@
 n = int(input())
 
-
-while True:
+while n > 0:
+    if n % 100 != 0 and n % 10 != 0 and n % 5 != 0:
+        print('Bad')
+        break 
     if n > 100:
-        print(n // 100)
+        print(f"100 - {n // 100}")
         n = n - (n // 100) * 100
     if n <= 100:
-        print(n // 10)
+        print(f"10 - {n // 10}")
         n = n - (n // 10) * 10
-    if n <= 5:
-        print(n // 5)
+    if n <= 5 and n != 0:
+        print(f"5 - {n // 5}")
         n = n - (n // 5) * 5
-    if n <= 0:
-        break
+
+x = 10
+
+y = 20 
+
+print(x is y)
