4b6f9c28fffcddaf97b19d4d3230104d3b1a0e85
ersildan <11free11@mail.ru>
2025-10-07 16:53:51 +0300

    +

--- Python_leetCode/118.py
+++ Python_leetCode/118.py
@@ -1,11 +1,13 @@
-n = int(input())
-lst = []
-for i in range(n):
-    temp_list = []
-    for j in range(i + 1):
-        if j == 0 or j == i:
-            temp_list.append(1)
-        else:
-            temp_list.append(lst[i - 1][j - 1] + lst[i - 1][j])
-    lst.append(temp_list)
-print(lst)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        lst = []
+        for i in range(numRows):
+            temp_list = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    temp_list.append(1)
+                else:
+                    temp_list.append(lst[i - 1][j - 1] + lst[i - 1][j])
+            lst.append(temp_list)
+        return lst
+    
